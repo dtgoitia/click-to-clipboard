160c6ca834bbe46695f4071501e67d742ef32001
David Torralba Goitia <david.torralba.goitia@gmail.com>
2021-11-11 08:41:29 +0000

    Copy text to clipboard on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+async function copyToClipboard(content: string): Promise<void> {
+  return navigator.clipboard.writeText(content);
+}
+
+const MOCK_VALUES = ["text 1", "text 2 with more stuff"];
+
+interface ClickMeButtonProps {
+  text: string;
+}
+function ClickMeButton({ text }: ClickMeButtonProps) {
+  return <div onClick={() => copyToClipboard(text)}>Click me to copy "{text}"</div>;
+}
+
 export default function App() {
-  return <h1>I'm a react app</h1>;
+  const [values, setValues] = useState(MOCK_VALUES);
+
+  return (
+    <div>
+      <p>Click any below to copy them to the clipboard</p>
+      {values.map((text, i) => (
+        <ClickMeButton key={i} text={text} />
+      ))}
+    </div>
+  );
 }
